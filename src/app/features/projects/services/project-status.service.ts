a9ab571fb2bb56758ddf181d0b24a760d8e167ff
David <davidmh.mail@gmail.com>
2019-12-19 00:23:52 -0700

    Implement snackbar notifcations on status update with status colors

--- src/app/features/projects/services/project-status.service.ts
+++ src/app/features/projects/services/project-status.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
-import { shareReplay } from 'rxjs/operators';
+import { shareReplay, tap } from 'rxjs/operators';
 import { ConfigService } from '../../../shared/services/config.service';
 import Project from '../interfaces/project.interface';
 import { WebsocketService } from './websocket.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import ProjectsStatusOverview from '../interfaces/projects-status-overview.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -22,11 +24,15 @@ export class ProjectStatusService {
 
     constructor(private http: HttpClient,
                 private configService: ConfigService,
-                private wsService: WebsocketService) {
+                private wsService: WebsocketService,
+                private snackBar: MatSnackBar) {
         const {projectsMonitorSubject, stopProjectsMonitorSubject, projectsMonitorCountdown$} = wsService.connect();
 
         this.projectsMonitorSocketSubject = projectsMonitorSubject;
-        this.projectsStatusMonitor$ = this.projectsMonitorSocketSubject.asObservable().pipe(shareReplay(1));
+        this.projectsStatusMonitor$ = this.projectsMonitorSocketSubject.asObservable().pipe(
+            tap(projectsStatus => this.openNotification(projectsStatus)),
+            shareReplay(1)
+        );
 
         this.stopProjectsMonitorSubject = stopProjectsMonitorSubject;
         this.stopProjectsMonitor$ = this.stopProjectsMonitorSubject.asObservable().pipe(shareReplay(1));
@@ -44,6 +50,27 @@ export class ProjectStatusService {
         console.log('Stopped monitoring projects');
         this.projectsMonitorOn = false;
         this.stopProjectsMonitorSubject.next(true);
+    }
+
+    private openNotification(projectsStatus: ProjectsStatusOverview) {
+        let msg = 'STATUS UPDATE';
+        const warning = Object.values(projectsStatus).some(status => status.warning);
+        const down = Object.values(projectsStatus).every(status => !status.up);
+        let statusClass = 'snackbar-status-update-up';
+        if (warning && !down) {
+            statusClass = 'snackbar-status-update-warning';
+            msg += ' - Something is down 😯';
+        } else if (down) {
+            statusClass = 'snackbar-status-update-down';
+            msg += ' - All projects are down!! 😭';
+        } else {
+            msg += ' - Projects are Up 👍';
+        }
+        this.snackBar.open(msg, null, {
+            duration: 5000,
+            verticalPosition: 'top',
+            panelClass: ['snackbar-status-update', statusClass]
+        });
     }
 
 
